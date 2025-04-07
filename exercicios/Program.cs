30bf49d5023425d8da1b94a2cc6c5c21d3d53822
GUSTAVO RIBEIRO DE OLIVEIRA <gustavo.oliveira06@cs.up.edu.br>
2025-04-07 20:08:28 -0300

    atividade 1

---
+++ exercicios/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+1. Exercício: Nome e Saudação,
+Peça ao usuário que digite seu nome e, em seguida, exiba uma saudação personalizada
+utilizando o nome inserido
+*/
+Console.WriteLine("Digite o seu nome: "); //Exibe mensagem pedindo o nome do aluno
+string nome = Console.ReadLine(); //Lê o nome digitado e armazena a variável "nome"
+
+Console.WriteLine(nome + " muito obrigado por rodar meu codigo !!! ");
